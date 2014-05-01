3a7cd528a26404764d1c7ff7a1a091a69a2a3798
Erin Altenhof-Long <ealtenho@wellesley.edu>
2014-05-01 15:02:59 -0400

    Updates

--- reflect.js
+++ reflect.js
@@ -12,50 +12,5 @@
 
 
 function saveToJournal {
-  var currentJournal = JSON.parse(localStorage.getItem("journalDetails"));
-    console.log("Current journal: " + currentJournal);
-     var lastEntry = currentJournal[currentJournal.length - 1].split("|");
-     var hasEntry = true;
-     var last = lastEntry[0].substring(0,9);
-     var isFirst = false;
-      if(last.indexOf("No") != -1)
-      {
-        isFirst = true;
-      }
-     console.log("This is the last entry date: " + last);
-     console.log(todaysDate());
-     console.log("Is the last entry date equal: " + (last == todaysDate()));
-     if(last != todaysDate())
-     {
-       hasEntry = false;
-       lastEntry = [todaysDate(), "", "", ""];
-     }
 
-     var arrayUpdate = "<br>" + div.val();
-
-    //currentCompleted.push(arrayUpdate);
-    lastEntry[1] = lastEntry[1] + arrayUpdate;
-    var str = "";
-    for(var i = 0; i < lastEntry.length - 1; i++)
-    {
-      str += lastEntry[i] + " | ";
-    }
-    str += lastEntry[lastEntry.length - 1];
-
-    console.log("Last entry: " + lastEntry);
-
-    if(hasEntry || isFirst)
-    {
-      currentJournal[currentJournal.length - 1] = str;
-    }
-    else
-    {
-      currentJournal.push(str);
-    }
-    localStorage.setItem("journalDetails", JSON.stringify(currentJournal));
-
-    //This should be open journal popup
-    alert("This reflection has been added to your journal!");
-    $("body").pagecontainer( "change", "#home");
-    makeLists();
 }
